9e13bf8ec0df251bf2c1de2933830b1a3ceadda8
Wynston Hou <houyewei@users.noreply.github.com>
2014-12-15 15:34:27 +0800

    Update block.js

--- block.js
+++ block.js
@@ -4,7 +4,7 @@
 
  chrome.webRequest.onBeforeRequest.addListener(
         function(details) {
-          return {cancel: details.url.indexOf("www.kuwo.cn/yy/PlayCheckIp") != -1};
+          return {cancel: true};
         },
-        {urls: ["<all_urls>"]},
+        {urls: ["http://webstat.kuwo.cn/webpv/www.jpg","http://www.kuwo.cn/yy/PlayCheckIp*","http://webstat.kuwo.cn/npv/all.jpg*","http://webstat.kuwo.cn/logtj/comm/commjstj/webmusic/web_player_ip.jpg*","http://webstat.kuwo.cn/logtj/comm/webplayer/playinit.jpg*","http://hm.baidu.com/hm.gif*","http://webstat.kuwo.cn/npv/webmusic.jpg","http://wa.kuwo.cn/lyrics/img/kwgg/kwgg_372.js","http://wa.kuwo.cn/lyrics/img/kwgg/kwgg_212.js","http://webstat.kuwo.cn/logtj/comm/commjstj/webmusic/play_song_sum.jpg*"]},
         ["blocking"]);
